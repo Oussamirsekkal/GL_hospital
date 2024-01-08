c4a834b6fca92af7020bd3de3867c8b98ff74211
Oussamirsekkal <142433285+Oussamirsekkal@users.noreply.github.com>
2024-01-08 23:43:27 +0100

    utilisateur and compte classes done

---
+++ models/compte.java
@@ -0,0 +1,42 @@
+package models;
+
+public class compte {
+    private int compteId; 
+    private int utilisateurId; 
+    private utilisateur utilisateur; 
+    private String accountType; 
+
+
+    public int getCompteId() {
+        return compteId;
+    }
+
+    public void setCompteId(int compteId) {
+        this.compteId = compteId;
+    }
+
+    public int getUtilisateurId() {
+        return utilisateurId;
+    }
+
+    public void setUtilisateurId(int utilisateurId) {
+        this.utilisateurId = utilisateurId;
+    }
+
+    public utilisateur getUtilisateur() {
+        return utilisateur;
+    }
+
+    public void setUtilisateur(utilisateur utilisateur) {
+        this.utilisateur = utilisateur;
+    }
+
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+}
---
+++ models/utilisateur.java
@@ -0,0 +1,59 @@
+package models;
+
+public class utilisateur {
+    private int utilisateurId; 
+    private String username; 
+    private String passwordHash; 
+    private String email; 
+    private String fullName; 
+    private String dateOfBirth; 
+
+
+    public int getUtilisateurId() {
+        return utilisateurId;
+    }
+
+    public void setUtilisateurId(int utilisateurId) {
+        this.utilisateurId = utilisateurId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
